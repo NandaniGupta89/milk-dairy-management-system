65a4787cf48b7becd571e5d6d4e6077f9ef9e05e
NandaniGupta89 <141022851+NandaniGupta89@users.noreply.github.com>
2023-10-14 13:39:06 +0530

    Add files via upload

---
+++ MilkDairyManagementSystem_VB.NET/MilkDairyManagementSystemVB/MilkDairyManagementSystemVB/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub button1_Click(sender As Object, e As EventArgs) Handles button1.Click
+        If textBox1.Text = "ruchisingh" And textBox2.Text = "billaop" Then
+            MessageBox.Show("Welcome Admin")
+
+
+            textBox1.Text = ""
+            textBox2.Text = ""
+            Hide()
+            Home.ShowDialog()
+
+        Else
+            MessageBox.Show("login fail")
+        End If
+
+    End Sub
+End Class
